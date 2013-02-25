627091b47ef400e3ab58e115a0e67524dbcf439b
nikbracic <nikbracic@yahoo.com>
2013-02-25 19:30:47 +0100

    Uploading files to a folder UPDATE

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,22 +11,20 @@ class HomeController < ApplicationController
 
   #this action is for viewing folders
   def browse
-    #get the folders owned/created by the current_user
-    @current_folder = current_user.folders.find(params[:folder_id])
+    #first find the current folder within own folders
+    @current_folder = current_user.folders.find_by_id(params[:folder_id])
 
     if @current_folder
 
-      #getting the folders which are inside this @current_folder
+      #show folders under this current folder
       @folders = @current_folder.children
 
+      #show only files under this current folder
       @assets = @current_folder.assets.order("uploaded_file_file_name desc")
-
-      #We need to fix this to show files under a specific folder if we are viewing that folder
-      @assets = current_user.assets.order("uploaded_file_file_name desc")
 
       render :action => "index"
     else
-      flash[:error] = "Don't be cheeky! Mind your own folders!"
+      flash[:error] = "Don't be cheeky! Mind your own assets!"
       redirect_to root_url
     end
   end
